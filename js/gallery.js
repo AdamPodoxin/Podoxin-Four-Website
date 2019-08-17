f61192fb9e4a9a4a4434d10d6768c96ff2162f93
Adam Pod <adam.podoxin@gmail.com>
2019-08-17 13:47:24 -0700

    Implemented jQuery in gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -2,43 +2,39 @@ var amountOfImages = 68;
 var currentImageIndex;
 
 function LoadImages() {
-	var galleryDiv = document.getElementById("gallery");
 	var src = "../img/gallery/";
 
 	for(var i = 0; i < amountOfImages; i++) {
 		var imageElement = document.createElement("img");
-		gallery.appendChild(imageElement);
+		$("#gallery").append(imageElement);
 
 		var imgName = "000" + (i + 1);
 		imgName = imgName.substring(imgName.length - 3, imgName.length);
 
-		imageElement.src = src + imgName + ".jpg";
-		imageElement.className = "photo";
-
-		if(window.innerWidth > 1280) {
-			imageElement.onclick = function() { OpenModal(this); };
-		}
+		$(imageElement).attr("src", src + imgName + ".jpg");
+		$(imageElement).attr("class", "photo");
+		$(imageElement).attr("img-index", i);
 	}
 }
 
 function OpenModal(image) {
-	modal.style.display = "block";
-	modalbg.style.display = "block";
+	$("#modal").css("display", "block");
+	$("#modalbg").css("display", "block");
 
-	modal.src = image.src;
-	currentImageIndex = parseInt(image.src.replace(/^.*[\\\/]/, '').replace('.jpg', '')) - 1;
+	$("#modal").attr("src", image.src);
+	currentImageIndex = parseInt($(image).attr("img-index"));
 
 	AdjustModalSize();
 	AdjustNavigationButtons();
 
-	document.documentElement.style.overflowY = "hidden";
+	$("html").css("overflowY", "hidden");
 }
 
 function CloseModal() {
-	modal.style.display = "none";
-	modalbg.style.display = "none";
+	$("#modal").css("display", "none");
+	$("#modalbg").css("display", "none");
 
-	document.documentElement.style.overflowY = "scroll";
+	$("html").css("overflowY", "scroll");
 }
 
 function ChangeImage(count) {
@@ -49,10 +45,9 @@ function ChangeImage(count) {
 	} else if(newImageIndex < 0) {
 		newImageIndex = amountOfImages - 1;
 	}
-	console.log(newImageIndex);
 
 	if(newImageIndex >= 0 && newImageIndex < amountOfImages) {
-		modal.src = gallery.children[newImageIndex].src;
+		$("#modal").attr("src", gallery.children[newImageIndex].src);
 		currentImageIndex = newImageIndex;
 
 		AdjustModalSize();
@@ -61,19 +56,45 @@ function ChangeImage(count) {
 }
 
 function AdjustModalSize() {
-	if(modal.clientWidth > window.innerWidth * 0.8) {
-		modal.style.width = "80%";
-		modal.style.height = "auto";
+	if($("#modal").attr("clientWidth") > window.innerWidth * 0.8) {
+		$("#modal").css("width", "80%");
+		$("#modal").css("height", "auto");
 	} 
 
-	if(modal.clientHeight > window.innerHeight * 0.8) {
-		modal.style.height = "80%";
-		modal.style.width = "auto";
+	if($("#modal").attr("clientHeight") > window.innerHeight * 0.8) {
+		$("#modal").css("height", "80%");
+		$("#modal").css("width", "auto");
 	}
 }
 
 function AdjustNavigationButtons() {
 	var distance = (window.innerWidth - modal.clientWidth) / 2 - 50;
-	prev.style.left = distance + "px";
-	next.style.right = distance + "px";
+	$("#prev").css("left", distance + "px");
+	$("#next").css("right", distance + "px");
 }
+
+$(document).ready(function() {
+	LoadImages();
+
+	$("img").click(function(event) {
+		if(window.innerWidth >= 1280 && $(event.target).attr("class") == "photo") {
+			OpenModal(event.target);
+		}
+	});
+
+	$("#modal").click(function() {
+		CloseModal();
+	});
+
+	$("#close").click(function() {
+		CloseModal();
+	});
+
+	$("#prev").click(function() {
+		ChangeImage(-1);
+	});
+
+	$("#next").click(function() {
+		ChangeImage(1);
+	});
+});
