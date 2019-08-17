36b96a8f42fa704c59546052661b80eaceda67e8
Adam Pod <adam.podoxin@gmail.com>
2019-08-17 13:10:55 -0700

    Implemented jQuery in index.js

--- js/index.js
+++ js/index.js
@@ -1,25 +1,35 @@
 var openPath = "img/navbar.png";
 var closePath = "img/modal/close.png";
 
+var isNavbarOpen = false;
+
 if(!document.title.includes("Home")) {
 	openPath = "../img/navbar.png";
 	closePath = "../img/modal/close.png";
 }
 
 function OpenNavbar() {
-	navbar.style.animation = "opennavbar 0.75s 1";
-	navbar.style.animationFillMode = "forwards";
+	$("#navbar").css("animation", "opennavbar 0.75s 1");
+	$("#navbar").css("animationFillMode", "forwards");
+	$("#navbar-button").attr("src", closePath);
 
-	var navbarButton = document.getElementById("navbar-button");
-	navbarButton.onclick = function() {CloseNavbar();};
-	navbarButton.src = closePath;
+	isNavbarOpen = true;
 }
 
 function CloseNavbar() {
-	navbar.style.animation = "closenavbar 0.75s 1";
-	navbar.style.animationFillMode = "forwards";
+	$("#navbar").css("animation", "closenavbar 0.75s 1");
+	$("#navbar").css("animationFillMode", "forwards");
+	$("#navbar-button").attr("src", openPath);
 
-	var navbarButton = document.getElementById("navbar-button");
-	navbarButton.onclick = function() {OpenNavbar();};
-	navbarButton.src = openPath;
+	isNavbarOpen = false;
 }
+
+$(document).ready(function() {
+	$("#navbar-button").click(function() {
+		if(isNavbarOpen) {
+			CloseNavbar();
+		} else {
+			OpenNavbar();
+		}
+	});
+});
