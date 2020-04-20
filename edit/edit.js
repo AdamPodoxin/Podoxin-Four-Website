f45d2203b5cc410a4dcc381c202c7f654933cfcd
Adam Podoxin <adam.podoxin@gmail.com>
2020-04-19 19:49:31 -0700

    Implemented JSON generation

--- edit/edit.js
+++ edit/edit.js
@@ -1,11 +1,14 @@
 let tabs = [];
+
 var eventsTable;
+var generatedJSON;
 
 const eventsTrTemplate = "<tr><td><input type='checkbox' name='finished'></td><td><input type='text' name='venue'></td><td><input type='text' name='date'></td></tr>";
 
 function onload() {
     tabs = document.getElementsByClassName("tab");
     eventsTable = document.getElementById("events-table");
+    generatedJSON = document.getElementById("generated-json");
 }
 
 function openTab(tabId) {
@@ -20,6 +23,14 @@ function openTab(tabId) {
 
 /*--------Events--------*/
 
+class EventsTableRow {
+    constructor(finished, venue, date) {
+        this.finished = finished;
+        this.venue = venue;
+        this.date = date;
+    }
+}
+
 function addEventsTr(finished, venue, date) {
     const firstChild = eventsTable.childNodes[0];
     const elementToAdd = $(eventsTrTemplate)[0];
@@ -33,6 +44,29 @@ function addEventsTr(finished, venue, date) {
     finishedInput.checked = finished;
     venueInput.value = venue;
     dateInput.value = date;
+}
+
+function generateEventsJSON() {
+    const tableRows = Array.from(eventsTable.querySelectorAll("tr"));
+    tableRows.pop();
+
+    let tableRowObjects = new Array();
+    tableRows.forEach(tableRow => {
+        const finished = tableRow.childNodes[0].childNodes[0].checked;
+        const venue = tableRow.childNodes[1].childNodes[0].value;
+        const date = tableRow.childNodes[2].childNodes[0].value;
+
+        const tableRowObject = new EventsTableRow(finished, venue, date);
+        tableRowObjects.push(tableRowObject);
+    });
+
+    const generatedJSONString = JSON.stringify(tableRowObjects);
+
+    generatedJSON.innerHTML = generatedJSONString;
+    generatedJSON.select();
+    generatedJSON.setSelectionRange(0, 99999);
+
+    document.execCommand("copy");
 }
 
 /*----------------------*/
