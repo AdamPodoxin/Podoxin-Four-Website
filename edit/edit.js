518a34d011680e2067c6197aeaf1c8853aae338e
Adam Podoxin <adam.podoxin@gmail.com>
2020-04-19 20:16:33 -0700

    Added missing inputs

--- edit/edit.js
+++ edit/edit.js
@@ -3,7 +3,7 @@ let tabs = [];
 var eventsTable;
 var generatedJSON;
 
-const eventsTrTemplate = "<tr><td><input type='checkbox' name='finished'></td><td><input type='text' name='venue'></td><td><input type='text' name='date'></td></tr>";
+const eventsTrTemplate = "<tr><td><input type='checkbox' name='finished'></td><td><input type='text' name='date'></td><td><input type='text' name='venue'></td><td><input type='text' name='venue-site'></td><td><input type='text' name='yt-id'></td></tr>";
 
 function onload() {
     tabs = document.getElementsByClassName("tab");
@@ -24,26 +24,32 @@ function openTab(tabId) {
 /*--------Events--------*/
 
 class EventsTableRow {
-    constructor(finished, venue, date) {
+    constructor(finished, date, venue, venueSite, ytId) {
         this.finished = finished;
         this.venue = venue;
+        this.venueSite = venueSite;
         this.date = date;
+        this.ytId = ytId;
     }
 }
 
-function addEventsTr(finished, venue, date) {
+function addEventsTr(finished, date, venue, venueSite, ytId) {
     const firstChild = eventsTable.childNodes[0];
     const elementToAdd = $(eventsTrTemplate)[0];
 
     eventsTable.insertBefore(elementToAdd, firstChild);
 
     const finishedInput = elementToAdd.childNodes[0].childNodes[0];
-    const venueInput = elementToAdd.childNodes[1].childNodes[0];
-    const dateInput = elementToAdd.childNodes[2].childNodes[0];
+    const dateInput = elementToAdd.childNodes[1].childNodes[0];
+    const venueInput = elementToAdd.childNodes[2].childNodes[0];
+    const venueSiteInput = elementToAdd.childNodes[3].childNodes[0];
+    const ytIdInput = elementToAdd.childNodes[4].childNodes[0];
 
     finishedInput.checked = finished;
-    venueInput.value = venue;
     dateInput.value = date;
+    venueInput.value = venue;
+    venueSiteInput.value = venueSite;
+    ytIdInput.value = ytId;
 }
 
 function generateEventsJSON() {
@@ -53,10 +59,12 @@ function generateEventsJSON() {
     let tableRowObjects = new Array();
     tableRows.forEach(tableRow => {
         const finished = tableRow.childNodes[0].childNodes[0].checked;
-        const venue = tableRow.childNodes[1].childNodes[0].value;
-        const date = tableRow.childNodes[2].childNodes[0].value;
+        const date = tableRow.childNodes[1].childNodes[0].value;
+        const venue = tableRow.childNodes[2].childNodes[0].value;
+        const venueSite = tableRow.childNodes[3].childNodes[0].value;
+        const ytId = tableRow.childNodes[4].childNodes[0].value;
 
-        const tableRowObject = new EventsTableRow(finished, venue, date);
+        const tableRowObject = new EventsTableRow(finished, date, venue, venueSite, ytId);
         tableRowObjects.push(tableRowObject);
     });
 
@@ -67,6 +75,7 @@ function generateEventsJSON() {
     generatedJSON.setSelectionRange(0, 99999);
 
     document.execCommand("copy");
+    alert("JSON copied to clipboard");
 }
 
 /*----------------------*/
