42daea333eeba382bcf2322915d8bfaff2e52819
Adam Podoxin <adam.podoxin@gmail.com>
2023-06-07 23:35:13 -0700

    Fix useIsMobile hook

--- src/lib/hooks/useIsMobile.ts
+++ src/lib/hooks/useIsMobile.ts
@@ -3,14 +3,16 @@ import { useEffect, useState } from "react";
 const useIsMobile = () => {
 	const [isMobile, setIsMobile] = useState(false);
 
+	const handleResize = () => {
+		setIsMobile(window.innerWidth < 768);
+	};
+
 	useEffect(() => {
-		const handleResize = () => {
-			setIsMobile(window.innerWidth < 768);
-		};
+		handleResize();
 
 		window.addEventListener("resize", handleResize);
 		return () => window.removeEventListener("resize", handleResize);
-	}, []);
+	});
 
 	return isMobile;
 };
